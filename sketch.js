ec950036671de9c0eb07d3770ce97dba3f8f4ae1
imthiyaz456 <92167268+imthiyaz456@users.noreply.github.com>
2021-11-05 19:36:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var balloon,balloonImage1,balloonImage2;
+// create database and position variable here
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  textSize(20); 
+}
+const firebaseConfig = {
+  apiKey: "AIzaSyB86wLGiA63INeddvLikZbe19YbA_Tc0jM",
+  authDomain: "multiplayer-air-ballon-game.firebaseapp.com",
+  projectId: "multiplayer-air-ballon-game",
+  storageBucket: "multiplayer-air-ballon-game.appspot.com",
+  messagingSenderId: "275088171328",
+  appId: "1:275088171328:web:e1634ab2aa14bbb9f65aa5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in left direction
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in right direction
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in up direction
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in down direction
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
